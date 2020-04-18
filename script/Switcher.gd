490db93dc7e588070939dc0e528094a791f7de15
ladroid <puatachok@gmail.com>
2020-04-18 23:43:06 +0300

    improve game

--- script/Switcher.gd
+++ script/Switcher.gd
@@ -1,17 +1,32 @@
 extends StaticBody2D
 
-var on = true
+var on = false
+var off = false
 
 func _ready():
-	pass 
+	pass
 
+func _physics_process(delta):
+	pass
+
+func switcher_on_off():
+	if on == true and off == false:
+		print("A")
+		$AnimatedSprite.play("on")
+		on = false
+		off = true
+	if on == false and off == true:
+		print("B")
+		$AnimatedSprite.play("off")
+		on = true
+		off = false
+	if on == false and off == false:
+		$AnimatedSprite.play("idle")
+		on = true
+		off = true
+		print("C")
 
 func _on_Area2D_body_entered(body):
 	if "Hero" in body.name:
-		if on == true:
-			$AnimatedSprite.play("on")
-			on = false
-		if on == false:
-			$AnimatedSprite.play("off")
-			on = true
+		switcher_on_off()
 	
